4d63046617ea9a1b3167c47907cd387ed05ce2df
Roger Stringer <roger.stringer@lge.com>
2013-06-19 15:18:22 -0700

    Reduce logging by switching off MOJ_DEBUG build flag
     - Remove MOJ_DEBUG build flag from compiler build flags
     - Correct use of MOJ_DEBUG_LOGGING to MOJ_DEBUG in MojDbIndex.cpp
    
    Open-webOS-DCO-1.0-Signed-off-by: Roger Stringer <roger.stringer@lge.com>
    
    Change-Id: Ic691d419d3d35f29418ebe2be273987a39744808
    Reviewed-on: https://g2g.palm.com/2691
    Reviewed-by: Build Verification
    Tested-by: Roger Stringer <roger.stringer@lge.com>
    Reviewed-by: Igor Kuksyuk <ikuksyuk@luxoft.com>
    Tested-by: Igor Kuksyuk <ikuksyuk@luxoft.com>
    Reviewed-by: Roger Stringer <roger.stringer@lge.com>

--- src/db/MojDbIndex.cpp
+++ src/db/MojDbIndex.cpp
@@ -603,7 +603,7 @@ MojErr MojDbIndex::delKeys(const KeySet& keys, MojDbStorageTxn* txn, bool forced
 	for (KeySet::ConstIterator i = keys.begin(); i != keys.end(); ++i) {
 
 		MojErr err = m_index->del(*i, txn);
-#if defined(MOJ_DEBUG_LOGGING)
+#if defined(MOJ_DEBUG)
 		char s[1024];
 		char *s2 = NULL;
 		if (m_kind)
@@ -619,7 +619,7 @@ MojErr MojDbIndex::delKeys(const KeySet& keys, MojDbStorageTxn* txn, bool forced
 		// This has some potential risk
 		if (err == MojErrInternalIndexOnDel) {
 			m_delMisses++;
-#if defined(MOJ_DEBUG_LOGGING)
+#if defined(MOJ_DEBUG)
 			MojLogWarning(s_log, _T("delKey %d for: %s - %s; key= %s; err = %d \n"), count+1, s2, this->m_name.data(), s, err);
 #endif
 			if (forcedel)
@@ -640,7 +640,7 @@ MojErr MojDbIndex::insertKeys(const KeySet& keys, MojDbStorageTxn* txn)
 	for (KeySet::ConstIterator i = keys.begin(); i != keys.end(); ++i) {
 
 		MojErr err = m_index->insert(*i, txn);
-#if defined(MOJ_DEBUG_LOGGING)
+#if defined(MOJ_DEBUG)
 		char s[1024];
 		size_t size = (*i).size();
 		MojErr err2 = MojByteArrayToHex((*i).data(), size, s); 

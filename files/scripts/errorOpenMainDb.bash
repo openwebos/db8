1c29344c0c3bfb0f093aa01046774bc5caa29b96
Maksym Sditanov <maxim.sditanov@lge.com>
2013-12-17 11:04:04 +0200

    Backup maindb if database corrupted
    :Release Notes:
    Backup corrupted database before factory reset
    
    :Detailed Notes:
    If maindb corrupted, backup it and after backup store it
    for future analisys.
    
    :Testing Performed:
    Compiled and tested localy.
    
    :QA Notes:
    
    :Issues Addressed:
    [BHV-1557] We need to remain logs why factory reset happens
    
    Open-webOS-DCO-1.0-Signed-off-by: Maksym Sditanov <maxim.sditanov@lge.com>
    
    Change-Id: I55b76a8ab60b62e951ffc678b3a4318e90f5db6e
    Reviewed-on: https://g2g.palm.com/4900
    Reviewed-by: DCO Verification
    Tested-by: Maksym Sditanov <maxim.sditanov@lge.com>
    Reviewed-by: Oleg Yablokov <oleg.yablokov@lge.com>
    Reviewed-by: Olexandr Solomakha <oleksander.solomakha@lge.com>
    Reviewed-by: Maksym Sditanov <maxim.sditanov@lge.com>

--- files/scripts/errorOpenMainDb.bash
+++ files/scripts/errorOpenMainDb.bash
@@ -1,4 +1,19 @@
 #! /bin/bash
 
+# check available disk space
+BYTES_FREE=$(($(stat -f -c "%a*%S" /mnt/lg/cmn_data)))
+BYTES_THRESHOLD=$((100*1024*1024))  # 100M will be enough for database backup
+
+# if enough disk space, backup current database for feature analisys
+if [ "$BYTES_FREE" -ge "$BYTES_THRESHOLD" ]; then
+    mkdir -p /mnt/lg/cmn_data/db8
+    BACKUP_FILENAME="/mnt/lg/cmn_data/db8/corrupted_maindb.tar.bz2"
+    rm -f $BACKUP_FILENAME
+    tar cjf $BACKUP_FILENAME /var/db/main
+    chmod 400 $BACKUP_FILENAME
+else
+    PmLogCtl log DB8 crit "mojodb-luna [] DB8 DBGMSG {} [maindb_errorOpenmainDb.bash] No space left to store corrupted maindb"
+fi
+
 #do factory reset
 /usr/bin/luna-send -n 1 luna://com.webos.service.tv.systemproperty/doUserDefault '{}'
